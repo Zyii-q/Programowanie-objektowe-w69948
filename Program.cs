c2e7ffa4fdb103789a92c1989c13b47c92963253
w69948 <w69948@RA117B-21>
2024-10-24 15:02:21 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Zadanie 1
+
+//            Console.WriteLine("Podaj liczbę");
+//            var input = Console.ReadLine();
+
+//            if (int.TryParse(input, out int liczba))
+//            {
+//                if (liczba % 2 == 0)
+//                {
+//                    Console.WriteLine("Parzysta");
+//                }
+//                else
+//                {
+//                 Console.WriteLine("Nieparzysta");
+//                }
+//            }
+
+
+//Zadanie 2
+
+
+Console.WriteLine("Podaj liczbę, od której wypisze wszystkie parzyste liczby od 1 do tej liczby");
+
+    int N = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Parzyste liczby od 1 do {N}:");
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0);
+        {
+            Console.WriteLine(i);
+        }
+    }
